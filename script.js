def70440edd192c79a6e2f9c19d9992b5f1ca31b
CH042399 <ledorvaldivieso@protonmail.com>
2023-05-30 06:56:37 +0800

    Saving current progress

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let popup = document.querySelector('.popup');
+let submit = document.querySelector('.submitBtn');
 
-function openPopup() {
-  popup.classList.add("openPopup");
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  title = document.querySelector("#title");
+  author = document.querySelector("#author");
+  pages = document.querySelector("#pages");
+
+  myLibrary.push({
+    Title : title.value,
+    Author : author.value,
+    Pages : pages.value 
+  })
+
+  popup.classList.remove("openPopup");
 }
 
-function closePopup() {
-  popup.classList.remove("openPopup");
+function addBookToLibrary() {
+
+}
+
+// Popup function
+function openPopup() {
+  popup.classList.add("openPopup");
 }
